26d27b3b49958c9e35f5a44e3d21d9fac3e6c438
Brian <40767783+brishaw@users.noreply.github.com>
2018-09-06 11:37:09 -0400

    added new js functionality for firebase to store train times...

--- assets/js/app_v1.js
+++ assets/js/app_v1.js
@@ -23,76 +23,57 @@ var database = firebase.database();
 
 // Initial Variables (SET the first set IN FIREBASE FIRST)
 // Note remember to create these same variables in Firebase!
+var trainName = "";
+var trainDest = "";
+var firstTrain = "";
+var frequency = "";
+var minAway = "";
 
-// var trainName = "";
-// var trainDest = "";
-// var firstTrain = "";
-// var frequency = "";
-// var minAway = "";
-
+var testTest = "";
 // Click Button changes what is stored in firebase
 $("#click-button").on("click", function (event) {
     // Prevent the page from refreshing
     event.preventDefault();
 
     // Get inputs
-    var trainName = $("#train-name").val().trim();
-    var trainDest = $("#train-dest").val().trim();
-    var firstTrain = $("#first-train").val().trim();
-    var frequency = $("#freq").val().trim();
-    var minAway = "nothing";
+    trainName = $("#train-name").val().trim();
+    trainDest = $("#train-dest").val().trim();
+    firstTrain = $("#first-train").val().trim();
+    frequency = $("#freq").val().trim();
+    minAway = "nothing";
 
-    
+    testTest = frequency;
     // Change what is saved in firebase
-    var newTrain = {
+    database.ref().set({
         trainName: trainName,
         trainDest: trainDest,
         firstTrain: firstTrain,
         frequency: frequency,
         minAway: minAway
-    };
-
-    database.ref().push(newTrain);
-
-
+    });
 
     // Create a new post reference with an auto-generated id
-    // var newPostRef = database.ref().push();
-    // newPostRef.set({
-    //     testTest: testTest
-    // });
-
-    // Clears all of the text-boxes
-    $("#train-name").val("");
-    $("#train-dest").val("");
-    $("#first-train").val("");
-    $("#freq").val("");
-
-}); // end click-button function
+    var newPostRef = database.ref().push();
+    newPostRef.set({
+        testTest: testTest
+    });
+});
 
 // Firebase is always watching for changes to the data.
 // When changes occurs it will print them to console and html
-database.ref().on("child_added", function (childSnapshot) {
+database.ref().on("value", function (snapshot) {
 
     // Print the initial data to the console.
-    console.log("childSnapshot value: " + childSnapshot.val());
-
-    var trainName = childSnapshot.val().trainName;
-    var trainDest = childSnapshot.val().trainDest;
-    var firstTrain = childSnapshot.val().firstTrain;
-    var frequency = childSnapshot.val().frequency;
-    var minAway = childSnapshot.val().minAway;
-
-
+    console.log("snapshot value: " + snapshot.val());
 
     // Log the value of the various properties
-    console.log("childSnapshot trainName: " + childSnapshot.val().trainName);
-    console.log("childSnapshot trainDest: " + childSnapshot.val().trainDest);
-    console.log("childSnapshot firstTrain: " + childSnapshot.val().firstTrain);
-    console.log("childSnapshot frequency: " + childSnapshot.val().frequency);
-    console.log("childSnapshot minAway: " + childSnapshot.val().minAway);
+    console.log("snapshot trainName: " + snapshot.val().trainName);
+    console.log("snapshot trainDest: " + snapshot.val().trainDest);
+    console.log("snapshot firstTrain: " + snapshot.val().firstTrain);
+    console.log("snapshot frequency: " + snapshot.val().frequency);
+    console.log("snapshot minAway: " + snapshot.val().minAway);
 
-    // *****************************
+// *****************************
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
@@ -103,10 +84,10 @@ database.ref().on("child_added", function (childSnapshot) {
     );
 
     // Append the new row to the table
-    $("#train-schedule > tbody").prepend(newRow);
+    $("#train-schedule > tbody").append(newRow);
 
 
-    // *****************************
+// *****************************
 
 
 
@@ -114,25 +95,25 @@ database.ref().on("child_added", function (childSnapshot) {
     // Change the HTML
     // $("#train-data").text(snapshot.val().trainName + " | " + snapshot.val().destName + " | " + snapshot.val().firstTrain + " | " + snapshot.val().frequency);
 
+    
 
+// *****************************
 
-    // *****************************
-
-    // trying to populate the table...
+// trying to populate the table...
 
     // var items = snapshot.val();
 
     // var rows = "";
-
+    
     //     $.each(items, function(){
     //         rows += "<tr><td>" + snapshot.val().trainName + "</td><td>" + snapshot.val().destName + "</td><td>" + snapshot.val().firstTrain + "</td><td>" + snapshot.val().frequency + "</td></tr>" 
     //     });
-
+    
 
     //     $(rows).prependTo("#train-schedule tbody");
 
-    // *****************************
-
+   // *****************************
+    
     // If any errors are experienced, log them to console.
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
